0308a90aa49bc14d4ccf4d8ec1d67bdc04633735
1nn0 <rus.y2k@gmail.com>
2016-02-22 01:45:47 +0300

    minor changes

--- app/models.py
+++ app/models.py
@@ -1,14 +1,46 @@
-from app import db
+import flask.ext.whooshalchemy as whooshalchemy
+
+from app import app, db
 
 ROLE_USER = 0
 ROLE_ADMIN = 1
 
+
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    login = db.Column(db.String(100), unique = True)
-    password = db.Column(db.String(100), unique = True)
-    role = db.Column(db.Integer, default = ROLE_USER)
-    operations = db.relationship('Operation', backref = 'user_id', lazy = 'dynamic')
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True,)
+    login = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100), unique=True)
+    role = db.Column(db.Integer, default=ROLE_USER)
+    last_login = db.Column(db.DATETIME)
 
     def __repr__(self):
         return '<User %r>' % (self.login)
+
+
+class Operations(db.Model):
+    __tablename__ = 'operations'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    operation = db.Column(db.String(200))
+    timestamp = db.Column(db.TIMESTAMP)
+
+
+class Sprav(db.Model):
+    __tablename__ = 'tb1'
+    __searchable__ = ['dolzh', 'name', 'fname', 'tel_loc', 'kab']
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    dolzh = db.Column(db.String(100))
+    org = db.Column(db.String(100))
+    name = db.Column(db.String(100))
+    fname = db.Column(db.String(100))
+    sname = db.Column(db.String(100))
+    str = db.Column(db.String(100))
+    kor = db.Column(db.String(100))
+    kab = db.Column(db.String(100))
+    tel = db.Column(db.String(100))
+    tel_loc = db.Column(db.String(100))
+    tel_mob = db.Column(db.String(100))
+    email = db.Column(db.String(120))
+
+whooshalchemy.whoosh_index(app, Sprav)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,33 @@
-from app import app
+from flask import render_template, redirect, url_for, g
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
+from app import app, db, models, forms
+from config import MAX_SEARCH_RESULTS
+
+
+@app.before_request
+def before_request():
+    g.search_form = forms.SearchForm()
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html', title="Test", content=models.Sprav.query.all())
+
+@app.route('/search', methods=('GET', 'POST'))
+def search():
+    if not g.search_form.validate_on_submit():
+        return redirect(url_for('search_results', query='*'))
+    return redirect(url_for('search_results', query=g.search_form.search.data))
+
+@app.route('/search/<query>')
+def search_results(query):
+    results = models.Sprav.query.whoosh_search(query, MAX_SEARCH_RESULTS).all()
+    return render_template('search.html',
+                           query=query,
+                           results=results)
+
+admin = Admin(app, name='Справочник', template_mode='bootstrap3')
+admin.add_view(ModelView(models.Sprav, db.session))
