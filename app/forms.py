079ab1340b2724f8a6fa9f239d16b3330c28c3d7
1nn0 <rus.y2k@gmail.com>
2016-05-27 12:30:09 +0300

    Изменения в формах

--- app/forms.py
+++ app/forms.py
@@ -2,17 +2,29 @@ from flask.ext.wtf import Form
 from wtforms import StringField, TextAreaField, SelectField
 from wtforms.validators import DataRequired
 
+
 class SearchForm(Form):
     search = StringField('search', validators=[DataRequired()])
-#    category = SelectField(u'Фильтр', choices=[('name', 'Фамилия'), ('fname', 'Имя'), ('dolzh', 'Должность')])
+
+
+# category = SelectField(u'Фильтр', choices=[('name', 'Фамилия'), ('fname', 'Имя'), ('dolzh', 'Должность')])
 
 class CreateForm(Form):
     client = StringField('Клиент', validators=[DataRequired()])
     origin = StringField('Откуда', validators=[DataRequired()])
     destination = StringField('Куда', validators=[DataRequired()])
     capacity = StringField('Грузоподъемность', validators=[DataRequired()])
-    type = SelectField('Тип ТС', choices=[('Борт','Борт1'),('Зад','Зад1')])
+    type = SelectField('Тип ТС',
+                       choices=[('Борт', 'Борт'), ('Тент', 'Тент'), ('Конт.', 'Контейнер'), ('Фургон', 'Фургон'),
+                                ('Ц-М-Т.', 'Цельнометалл.'), ('Реф.', 'Рефрижератор'), ('Площ.', 'Площадка'),
+                                ('Шал.', 'Шаланда'), ('Трал', 'Трал'),
+                                ('Автовышка', 'Автовышка'), ('К.площ.', 'Контейнерная площадка'), ('Кран', 'Кран'),
+                                ('Лесовоз', 'Лесовоз'),
+                                ('Трубовоз', 'Трубовоз'), ('Манипулятор', 'Манипулятор'), ('Мега', 'Мега'),
+                                ('Погрузчик', 'Погрузчик'),
+                                ('Такелаж', 'Такелаж')])
     comments = TextAreaField('Примечания', validators=[DataRequired()])
+
 
 class EditForm(CreateForm):
     rate = StringField('Ставка')
